038dc225d983f740de73da6933a1fa76422d9f2b
Fenil412 <chodvadiyafenil@gmail.com>
2025-05-10 22:24:01 +0530

    Added

--- server/venv/Scripts/activate.bat
+++ server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\PROJECTS\Mined_Hackathon_2025\server\venv
+set VIRTUAL_ENV=D:\PROJECTS\Mined_Hackathon_2025\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
